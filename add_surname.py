b5bb5843528f5399f4194b532fcbb72ae1363524
Paul <quaifep@yahoo.com>
2021-02-12 23:49:35 -0800

    Initial commit

---
+++ add_surname.py
@@ -0,0 +1,8 @@
+# Author: Paul Quaife
+# Date: 2/12/2021
+# Description: Takes a list of numbers. The function should return the statistical median of those numbers.
+
+def add_surname(first_names_list):
+
+    full_list = [name+' Kardashian' for name in first_names_list if name[0] == 'K']
+    return full_list
